7fe364a8a7687c51a028fd541e1ae0d4529f3fd6
popovaludmila <57451404+popovaludmila@users.noreply.github.com>
2023-09-03 02:48:58 +0300

    При наведение меняется цвет кнопки

--- src/components/products/cart/cart.jsx
+++ src/components/products/cart/cart.jsx
@@ -1,15 +1,33 @@
 import { useDispatch } from "react-redux";
 import cartStyles from "./cart.module.css";
 import { addProduct } from "../../../store/slices/cartSlice";
+import { createRef, useState } from "react";
 
 export const Cart = ({product}) => {
-
+    const [isClicked, setIsClicked] = useState();
     const {src, price, name, weight} = product;
-   
+    const btnRef = createRef();
+
+    const activeStyle = {
+        backgroundColor: "#FFAB08",
+        color: "#FFFFFF"
+    };
+
+    const handleOnFocus = () => {
+        btnRef.current.style.backgroundColor = "#F86310";
+        btnRef.current.style.color = "#FFFFFF"; 
+    }
+            
+    const handleOnBlur = () => {
+        btnRef.current.style.backgroundColor = "#F2F2F3";
+        btnRef.current.style.color = "#000";
+    }
+
     const dispatch = useDispatch();
 
     const addProductToCart = () => {
-        dispatch(addProduct(product))
+        dispatch(addProduct(product));
+        setIsClicked(true);
     }
 
     return (
@@ -18,7 +36,14 @@ export const Cart = ({product}) => {
             <span className={cartStyles.price}>{price}₽</span>
             <p className={cartStyles.name}>{name}</p>
             <span className={cartStyles.weight}>{weight}г</span>
-            <button onClick={addProductToCart} className={cartStyles.btn}>Добавить</button>
+            <button 
+                onMouseEnter={handleOnFocus} 
+                onMouseLeave={handleOnBlur} 
+                onClick={addProductToCart} 
+                ref={btnRef} 
+                className={cartStyles.btn}>
+                {!isClicked ? "Добавить" :" Добавлено"}  
+            </button>
         </li>
     )
 }
